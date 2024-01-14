bf03a73d0ba7711b62563e0a89dd74cd2a1df4fa
Rodrigo Narvaez <rodadev47@gmail.com>
2024-01-14 12:22:51 -0500

    adding i18n and lang

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,18 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://rodrigonarvaez.com/",
-  integrations: [tailwind(), icon({
-    include: {
-      openmoji: ["linkedin", "github", "instagram"]
-    }
-  }), sitemap()]
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        openmoji: ["linkedin", "github", "instagram"],
+      },
+    }),
+    sitemap(),
+  ],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+    // locales: ["en", "es"],
+  },
 });
