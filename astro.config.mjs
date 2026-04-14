eaa50274f1b895ce3b9b2061e8088f52e62a922e
Rodrigo Narvaez <rodadev47@gmail.com>
2026-04-14 16:49:27 -0400

    added aria, fonts, and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rodrigonarvaez.com',
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
